356a02ff3f3d9e84a6a7757ddc5a4151baccd3ee
Juan Pablo Flores <jp@juanpabloflores.me>
2019-06-30 11:14:58 -0500

    Adds registration

--- js/main.js
+++ js/main.js
@@ -2,3 +2,15 @@
 $(function () {
     $('[data-toggle="popover"]').popover()
   })
+
+  $('.btn-inscripcion').click(function(){
+    Swal.fire({
+      title: 'REGISTRO',
+      background: '#18293A',
+      html: '<div class="row justify-content-around" id="popInscr">'+
+              '<button class="btn col-md-5 btn-gradient btn-mxhacks">Registro Conferencias</button>'+
+              '<button class="btn col-md-5 btn-gradient btn-it-show">¡Aplica al Hackaton!</button>'+
+            '</div>',
+      showConfirmButton: true,
+    })
+  });
