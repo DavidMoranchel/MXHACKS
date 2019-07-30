4faed5a00ec96f9ac2d30d2b04845eeb4c31d307
Juan Pablo <jp@juanpabloflores.me>
2019-07-30 01:46:05 -0500

    Fixes links

--- js/main.js
+++ js/main.js
@@ -8,8 +8,8 @@ $(function () {
       title: 'REGISTRO',
       background: '#18293A',
       html: '<div class="row justify-content-around" id="popInscr">'+
-              '<button class="btn col-md-5 btn-gradient btn-mxhacks" href="https://www.eventbrite.com/e/unamxhacks-conferencias-conisoft-2019-tickets-64383992254">Registro Conferencias</button>'+
-              '<button class="btn col-md-5 btn-gradient btn-it-show" href="https://registro.mxhacks.io/login">¡Aplica al Hackaton!</button>'+
+              '<a class="btn col-md-5 btn-gradient btn-mxhacks" target="_blank" href="https://www.eventbrite.com/e/unamxhacks-conferencias-conisoft-2019-tickets-64383992254">Registro Conferencias</a>'+
+              '<a class="btn col-md-5 btn-gradient btn-it-show" target="_blank" href="https://registro.mxhacks.io/login">¡Aplica al Hackaton!</a>'+
             '</div>',
       showConfirmButton: true,
     })
