c52dcf60a342cc03b578c3345476b14f833a4742
Juan Pablo Flores <jp@juanpabloflores.me>
2019-09-30 11:41:26 -0500

    adds buttons

--- js/main.js
+++ js/main.js
@@ -8,8 +8,9 @@ $(function () {
       title: 'REGISTRO',
       background: '#18293A',
       html: '<div class="row justify-content-around" id="popInscr">'+
-              '<a class="btn col-md-5 btn-gradient btn-mxhacks" target="_blank" href="https://www.eventbrite.com/e/unamxhacks-conferencias-conisoft-2019-tickets-64383992254">Registro Conferencias</a>'+
-              '<a class="btn col-md-5 btn-gradient btn-it-show" target="_blank" href="https://registro.mxhacks.io/login">¡Aplica al Hackaton!</a>'+
+              '<a class="btn col-md-5 btn-gradient btn-mxhacks" target="_blank" href="https://www.eventbrite.com/e/unamxhacks-conferencias-conisoft-2019-tickets-64383992254">Registro ITShowroom</a>'+
+              '<a class="btn col-md-5 btn-gradient btn-conisoft" target="_blank" href="#">Registro CONISOFT</a>'+
+              '<a class="btn col-md-5 btn-gradient btn-it-show mt-2" target="_blank" href="https://registro.mxhacks.io/login">¡Aplica al Hackaton!</a>'+
             '</div>',
       showConfirmButton: true,
     })
